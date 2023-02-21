ff6633461d8cbce4d5bd71fc435e04d698bccd82
michaeljumah <michaeljumah8@gmail.com>
2023-02-21 08:32:23 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * times_table -  a function that prints the 9 times table, starting with 0
+ * time_table -  a function that prints the 9 times table, starting with 0
  * rone = row, cone = column, d = digits of current result
- * Return: times table
+ * Return: time table
  * add extra space past single digit
  */
 void time_table(void)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void time_table(void);int add(int, int);
+void time_table(void);
+int add(int, int);
 void print_to_98(int n);
 void print_time_table(int n);
