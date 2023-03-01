316e84cb5e7223ca80d5d7e13ffe8d726c5fb043
michaeljumah <michaeljumah8@gmail.com>
2023-03-01 05:30:23 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+
+#endif /*MAIN_H */
