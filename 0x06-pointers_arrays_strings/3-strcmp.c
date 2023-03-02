54d3d6eb0e0ba38e32ca16e570e900e0fd967385
michaeljumah <michaeljumah8@gmail.com>
2023-03-01 23:43:36 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (s1[i] == s2[i])
+	while (*s1 != '\0')
+		s1++;
+	while (*s2 != '\0')
+		s2++;
+	if (*s1 == *s2)
 	{
-		if (s1[i] == '\0')
-			return (s1[i] - s2[i]);
-		i++;
+		return (0);
 	}
-
-	return (s1[i] - s2[i]);
+	return (*s1 == *s2);
 }
