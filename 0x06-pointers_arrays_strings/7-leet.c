71116ce8162b5028e273367508dc69ae41933976
michaeljumah <michaeljumah8@gmail.com>
2023-03-02 03:48:11 -0800

    s

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,27 +3,27 @@
 /**
  * leet - Write a function that encodes a string into 1337
  *
- * @s: This is the input string
+ * @changed: This is the input string
  *
  * Return: String converted to 1337
  */
 
-char *leet(char *s);
+char *leet(char *changed)
 {
-	int i, j;
-	char lower[] = {'a', 'e', 'o', 't', 'l', '\0'};
-	char upper[] = {'A', 'E', 'O', 'T', 'L', '\0'};
-	char num[] = {'4', '3', '0', '7', '1', '\0'};
+	int index, j;
+	char minus[] = {'a', 'e', 'o', 't', 'l', '\0'};
+	char mayus[] = {'A', 'E', 'O', 'T', 'L', '\0'};
+	char numbers[] = {'4', '3', '0', '7', '1', '\0'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (index = 0; changed[index] != '\0'; ++index)
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (s[1] == lower[j] || s[i] == upper[j])
+			if (changed[index] == minus[j] || changed[index] == mayus[j])
 			{
-				s[i] = num[j];
+				changed[index] = numbers[j];
 			}
 		}
 	}
-	return (s);
+	return (changed);
 }
