6bafddab92a1bba764b5a897313abf192e19b7d1
michaeljumah <michaeljumah8@gmail.com>
2023-03-02 01:36:19 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,13 @@
 char *string_toupper(char *l)
 {
 	int i;
-	int desp = 'a' - 'A';
+	/*int desp = 'a' - 'A';*/
 
 	for (i = 0; l[i] != '\0'; i++)
 	{
-		if (l[i] >= 'a' && l[i] <= 'z')
+		if (l[i] >= 97 && l[i] <= 122)
 		{
-			l[i] = l[i] - desp;
+			l[i] = l[i] - 32;
 		}
 		return (l);
 	}
