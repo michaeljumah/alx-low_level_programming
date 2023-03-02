4f31dc9534cbb0fb0fd163183feab295fb9a2e17
michaeljumah <michaeljumah8@gmail.com>
2023-03-02 03:06:01 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,12 +5,13 @@
  * @s: input string
  * Return: string converted to 1337
  */
+
 char *leet(char *s);
 {
 	int i, j;
-	char lower[] = {a,e,o,t,l};
-	char upper[] = {A,E,O,T,L};
-	char num[] = {4,3,0,7,1;}
+	char lower[] = {'a', 'e', 'o', 't', 'l'};
+	char upper[] = {'A', 'E', 'O', 'T', 'L'};
+	char num[] = {'4', '3', '0', '7', '1'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
