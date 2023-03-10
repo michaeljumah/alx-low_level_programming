465158a8c6a01f2119ec818b6f97f24347fc85ea
michaeljumah <michaeljumah8@gmail.com>
2023-03-10 15:43:21 -0800

    main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
+/**
+ * main - displays string
+ * Return - returns string
+ */
 int main(void)
 {
-	_puts("\"At thend of the,daygoalwastbeththacker\"\n\t -Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\n\t")
+	_puts("-Kevin Mitnick");
 	return (0);
 }
