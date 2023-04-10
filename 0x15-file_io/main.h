0f40335f2efdebe5ae88fa47c0393673efabad19
michaeljumah <michaeljumah8@gmail.com>
2023-04-10 01:57:42 -0700

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define BUFFER_SIZE 1024
+
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
