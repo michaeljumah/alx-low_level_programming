cd05fecdaa4153f8359ebb938403134e55b59601
michaeljumah <michaeljumah8@gmail.com>
2023-04-10 01:48:01 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,8 @@ int main(int argc, char **argv)
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage:cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage:cp file_from file_to\n")
+			exit(97);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
