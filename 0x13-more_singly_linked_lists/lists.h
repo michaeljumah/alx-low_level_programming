b60589bab30750909734725a0ddf2dfe67bc0a9e
michaeljumah <michaeljumah8@gmail.com>
2023-04-27 05:41:20 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -36,6 +36,7 @@ size_t looped_listint_count(listint_t *head);
 size_t free_listint_safe(listint_t **h);
 size_t print_listint_safe(const listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
-
+size_t listint_len(const listint_t *h);
+int check_ptr(const listint_t *ptr, listint_t **array, unsigned int size);
 
 #endif /*LISTS_H*/
