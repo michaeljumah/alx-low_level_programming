9f51c30f2f05949e6199535cafd1af76f5d3a8b1
michaeljumah <michaeljumah8@gmail.com>
2023-04-27 05:33:27 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,56 +1,5 @@
 #include "lists.h"
 
-size_t looped_listint_count(listint_t *head);
-size_t free_listint_safe(listint_t **h);
-
-/**
- * looped_listint_count - Counts the number of unique nodes
- *                      in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
- *
- * Return: If the list is not looped - 0.
- *         Otherwise - the number of unique nodes in the list.
- */
-size_t looped_listint_count(listint_t *head)
-{
-	listint_t *t, *ha;
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	t = head->next;
-	ha = (head->next)->next;
-
-	while (ha)
-	{
-		if (t == ha)
-		{
-			t = he;
-			while (t != ha)
-			{
-				nodes++;
-				t = t->next;
-				ha = ha->next;
-			}
-
-			t = t->next;
-			while (t != ha)
-			{
-				nodes++;
-				t = t->next;
-			}
-
-			return (nodes);
-		}
-
-		t = t->next;
-		ha = (ha->next)->next;
-	}
-
-	return (0);
-}
-
 /**
  * free_listint_safe - Frees a listint_t list safely (ie.
  *                     can free lists containing loops)
