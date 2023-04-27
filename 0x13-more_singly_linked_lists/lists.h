f803cda175890d8c83131866258c537f49e080cf
michaeljumah <michaeljumah8@gmail.com>
2023-04-27 05:03:27 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,7 +32,8 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h)
 
 
 
